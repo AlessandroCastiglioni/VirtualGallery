62d88954e371cee6d59403f520e81aefe6eb38e1
AlessandroCastiglioni <94126890+AlessandroCastiglioni@users.noreply.github.com>
2021-11-18 17:26:41 +0100

    Start HTML

---
+++ progetti/forniture/animation.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Virtual Gallery</title>
+    </head>
+    <body>
+    <p>
+        <h1><b>V</b></h1><h2>irtual</h2> <h1><b>G</b></h1><h2>allery</h2>
+    </p>
+    </body>
+</html>
